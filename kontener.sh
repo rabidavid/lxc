0110c79b16624abe560198f28b0976dbbe9319a4
Rabi Dávid <e12.rabi.david@palfy-vizugyi.hu>
2017-02-23 09:14:26 +0100

    Update kontener.sh

--- kontener.sh
+++ kontener.sh
@@ -2,5 +2,5 @@
 lxc init ubuntu:16.04 rabidavid
 lxc start rabidavid
 sleep 15
-lxc exec apt-get update --yes
-lxc exec apt-get upgrade --yes
+lxc exec rabidavid apt update -y
+lxc exec rabidavid apt upgrade -y
